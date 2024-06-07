88075128e077accc8c6272d770ea717edfde9b2a
George Popov <g8677607@gmail.com>
2024-06-07 07:56:32 +0300

    new change

--- src/main/java/HelloEverebody.java
+++ src/main/java/HelloEverebody.java
@@ -1,11 +1,16 @@
 public class HelloEverebody {
     public static void main(String[] args) {
+        System.out.println("GIT DEVELOP");
 
         System.out.println("Hello everybody");
 
         System.out.println("I am happy");
 
+
+        System.out.println("I am ");
+
         System.out.println("I dont");
+
 
     }
 }
